30c4f6eea5a572d0c24e1504a2843a43c3d7ee8c
Aravindhanl <54537639+Aravindhanl@users.noreply.github.com>
2021-07-15 02:25:27 +0530

    Update s2.py
    adding os module

--- s2.py
+++ s2.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 client = boto3.client('ec2',aws_access_key_id='AKIA2SITOGADH77K57M5',aws_secret_access_key='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', 
                       region_name= 'us-east-1')
