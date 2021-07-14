d2e3999193976a9297d29ee257dbfd2304747fbb
Aravindhan Loganathan <Aravindhan.Loganathan@lntinfotech.com>
2021-07-15 02:32:47 +0530

    adding sys

--- s2.py
+++ s2.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import sys
 
 client = boto3.client('ec2',aws_access_key_id='AKIA2SITOGADH77K57M5',aws_secret_access_key='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', 
                       region_name= 'us-east-1')
